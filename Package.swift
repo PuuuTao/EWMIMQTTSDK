422c4a305e43ded8b6e0d29c2156a487bd67a4d7
PuuuTao <1925134090@qq.com>
2023-11-15 11:13:48 +0800

    support for cocoapods

--- Package.swift
+++ Package.swift
@@ -5,17 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "EWMIMQTTSDK",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "EWMIMQTTSDK",
+            type: .dynamic,
             targets: ["EWMIMQTTSDK"]),
     ],
+    dependencies: [
+            .package(url: "https://github.com/emqx/CocoaMQTT", from: "2.1.8"),
+        ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "EWMIMQTTSDK"),
+            name: "EWMIMQTTSDK",
+            dependencies: [ "CocoaMQTT" ]),
         .testTarget(
             name: "EWMIMQTTSDKTests",
             dependencies: ["EWMIMQTTSDK"]),
